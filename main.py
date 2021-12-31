f50d2caf80e8f223fce93cba64264d5ab4d8c6e9
Hashan Dimuthu <91044348+HashanDimuthu@users.noreply.github.com>
2021-12-30 21:23:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ bot = telebot.Telebot(API_KEY)
 
 @bot.message_handler(commands=['start'])
 def great(message):
-    bot.reply_to(message, "𝓨𝓮𝓼 𝓘 𝓪𝓶 𝓗𝓮𝓻𝓮")
+    bot.reply_to(message, "𝓨𝓮𝓼 𝓘 𝓪𝓶 𝓗𝓮𝓻𝓮 /n/n")
 
 @bot.message_handler(commands=['hello'])
 def hello(message):
     bot.send_message(message.chat.id, "𝓗𝓮𝓵𝓵𝓸 𝓗𝓸𝔀 𝓪𝓻𝓮 𝔂𝓸𝓾?")
 
-@bot.message_ha
+@bot.message_handler(commands=[''])
+def (message):
 
 bot.polling()
